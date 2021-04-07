409287f05accddd2a2e23d635fee7903a56f479c
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-06 21:57:18 -0300

    nova query: trainer

--- src/typeDefs/query.js
+++ src/typeDefs/query.js
@@ -3,6 +3,7 @@ const {gql} = require("apollo-server");
 const query = gql`
     type Query {
         pokemons(generation: String,is_legendary: String, type1: String, type2: String, abilities: [String]):[Pokemon]
+        trainer(name: String): Trainer
     }
 `;
 
